433ffaa8b6c88649bd414ae0c275ab1f4af61b5e
Sir Austin <yuxm.austin2016@outlook.com>
2025-02-01 11:57:04 -0800

    init project

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # photo_picker
+
+
+
+TODOs
+
+- Add `poetry` as dependency management
+- Add `pre-commit`
---
+++ tests/test_demo.py
@@ -0,0 +1,5 @@
+import photo_picker as pp
+
+
+def test_getter():
+    assert pp.getter() == "photo_picker"
